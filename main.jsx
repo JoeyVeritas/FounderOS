df87a86265844500f57183ed60d57ca22a785d27
JoeyVeritas <132771492+JoeyVeritas@users.noreply.github.com>
2026-06-09 00:07:39 +0800

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import FounderOS from './FounderOS.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <FounderOS />
-  </React.StrictMode>
-)
